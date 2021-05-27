f83b8103eb95d3c421568379cd520a2de3ed399a
Irune <iarratibelrdaw1@gmail.com>
2021-05-27 12:57:56 +0200

    comentarios hechos

--- PiedraPapelTijera/src/ende/Juego.java
+++ PiedraPapelTijera/src/ende/Juego.java
@@ -1,6 +1,7 @@
 package ende;
 
 /**
+ * @author Irune Arratibel
  * Simula el juego de Piedra, Papel o tijera
  */
 
@@ -20,7 +21,7 @@ public class Juego
         Jugador p1 = new Jugador();
         Jugador p2 = new Jugador();
         boolean fin_de_juego = false;  
-        Integer Rondasjugadas = 0;    // Número de rondas jugadas
+        Integer Rondasjugadas = 0;    // Numero de rondas jugadas
         Integer EXITOS_jugador1 = p1.getExitos();
         Integer EXITOS_jugador2 = p2.getExitos();
         Integer EmPaTeS = 0;
